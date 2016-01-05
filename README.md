2310dc50bf63a135abe367d98d3859330b43460b
Dennis van den Brand <dennis.vandenbrand@nubisonline.nl>
2016-01-05 09:55:42 +0100

    Removed Django

--- README.md
+++
@@ -1,4 +0,0 @@
-# web-technology
-Assignment 1 of web technology
-# Installation
-Usuming you have python installed on your system. Install the requirements.txt file with pip. Then run python mange.py runserver 0.0.0.0:8080
--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url, patterns
-from api import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.HomePageView.as_view()),
-    url(r'^(?P<template>[\w-]+)$', views.HomePageView.as_view()),
-)
